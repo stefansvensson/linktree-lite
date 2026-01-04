f75603710107300dc915c22769e1be4e62dfe623
Stefan Svensson <stefansvensson91@gmail.com>
2026-01-04 20:56:28 +0100

    feat: add configuration file

---
+++ config.js
@@ -0,0 +1,46 @@
+// Configuration for Linktree-lite
+const config = {
+  // Personal information
+  name: "Stefan Svensson",
+  title: "Stefan Svensson",
+  email: "stefan@nevin.se",
+  
+  // Description
+  description: {
+    text: "Building things at",
+    company: {
+      name: "Nevin",
+      url: "https://www.nevin.se/"
+    }
+  },
+  
+  // Links
+  links: [
+    {
+      type: "email",
+      label: "Email",
+      hoverLabel: "Copy",
+      buttonId: "button-6c5k7wq1t",
+      email: "stefan@nevin.se"
+    },
+    {
+      type: "linkedin",
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/in/stefansvensson/",
+      ariaLabel: "LinkedIn"
+    },
+    {
+      type: "github",
+      label: "GitHub",
+      url: "https://github.com/stefansvensson",
+      ariaLabel: "GitHub"
+    }
+  ],
+  
+  // Assets
+  assets: {
+    avatar: "assets/avatar.svg",
+    favicon: "assets/favicon.svg"
+  }
+};
+
